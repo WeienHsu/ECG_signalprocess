6ea3c0630d66dc9b36c6a78e90c7cff316578803
weien <shiweien@gmail.com>
2018-02-02 02:17:59 +0800

    Update README.md

--- app/src/main/java/com/example/hsuweien/drawsignalline_06v4/drawlineView.java
+++ app/src/main/java/com/example/hsuweien/drawsignalline_06v4/drawlineView.java
@@ -58,6 +58,7 @@ public class drawlineView extends SurfaceView implements SurfaceHolder.Callback,
     private float old_x1=0, old_dist=0; //第一次觸碰時的座標
     private VelocityTracker mVelocityTracker = null;
 
+
     public drawlineView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
