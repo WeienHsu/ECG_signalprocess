7738f62a8560e637253d0e6ecbce3927a443fc87
weien <shiweien@gmail.com>
2018-02-02 01:24:47 +0800

    Update README.md

--- app/src/main/java/com/example/hsuweien/drawsignalline_06v4/drawlineView.java
+++ app/src/main/java/com/example/hsuweien/drawsignalline_06v4/drawlineView.java
@@ -52,7 +52,7 @@ public class drawlineView extends SurfaceView implements SurfaceHolder.Callback,
     private int LogarithmOrLinear_scale=1;    // 尺度選擇，預設線性尺度(1)
     private float Log_accuracy = 0.001f;   // Log尺度的精度，每格幾單位畫一點
 
-    //5555555
+
 
     // touch event
     private boolean setDragEnable = false; // 開啟手指拖移畫面？
